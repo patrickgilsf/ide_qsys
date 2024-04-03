8bcb77f3d586745f421a08b7589d1acddc551417
Patrick Gilligan <patrickgi@zillowgroup.com>
2024-04-02 19:08:20 -0700

    added lmethod to pull data from core, and push to file if desired

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import exp from 'constants';
 import fs from 'fs';
 import net from 'net';
+import jsonrpc from 'jsonrpc-lite';
+
 const timeoutPromise = (timeout) => new Promise((resolve) => setTimeout(resolve, timeout))
 
 
@@ -33,6 +35,7 @@ class Core {
     false;
   };
 
+  //parse string to push to core
   addCode = (comp, code, id, type) => {
     return JSON.stringify({
       "jsonrpc": "2.0",
@@ -50,8 +53,8 @@ class Core {
     })
   };
 
+  //update data to core
   update = async (input, options) => {
-
     //handle optional arguments
     let id, type;
     if (options) {
@@ -81,10 +84,85 @@ class Core {
         console.log(`updating ${this.comp}'s ${type} to ${input}`)
         client.write(this.addCode(this.comp, input, id, type) + this.nt);
       }
-
-
       client.on('data', (d) => {
         console.log(`received data from QRC API: ${d}`);
+      });
+      client.on('close', () => {
+        console.log('server closed connection');
+        client.end();
+      });
+      await timeoutPromise(3000);
+      client.end();
+    })
+  };
+
+  //parse string to pull from core
+  pullCode = (comp, id, type) => {
+    return JSON.stringify({
+      "jsonrpc": "2.0",
+      "id": id,
+      "method": "Component.Get",
+      "params": {
+        "Name": comp,
+        "Controls": [
+          {
+            "Name": type
+          }
+        ]
+      }
+    })
+  }
+
+  //pull data from core
+  retrieve = async (output, options) => {
+    //return obj
+    let rtn;
+    //handle optional arguments
+    let id, type;
+    if (options) {
+      id = options.id ? options.id : 1234;
+      type = options.type ? options.type : "code"
+    } else {
+      options = {};
+      id = 5678;
+      type = "code";
+    };
+    
+    options.type ? console.log(`type has been modified to: ${options.type}`) : null;
+    //handle login
+    let login = this.login();
+    login ? console.log("trying credentials....") : console.log("no credentials given");
+
+    let client = new net.Socket();
+
+    client.connect(1710, this.ip, async () => {
+      
+      this.login() ? client.write(login + this.nt) : null;
+
+      console.log(`retreiving ${this.comp}'s ${type}`)
+      client.write(this.pullCode(this.comp, id, type) + this.nt);
+
+      client.on('data', (d) => {
+        //convert from buffer to string, remove null termination
+        let str = Buffer.from(d, 'utf-8').toString().replace(/\x00/g, "")
+        //use jsonrpc library to parse string and print to console
+        for (let [a,b] of Object.entries(jsonrpc.parse(str))) {
+          if (b.result) {
+            rtn = b.result
+
+            console.log(`Here is your requested data:`);
+            console.log(rtn)
+          
+            //stream to file if selected
+            if (!output) {
+              console.log('no output file selected')
+            } else {
+              console.log(`creating file at ${output} with return data`)
+              let f = fs.createWriteStream(output);
+              f.write(JSON.stringify(rtn, null, 2));
+            }          
+          }
+        };
       });
 
       client.on('close', () => {
@@ -95,9 +173,7 @@ class Core {
       await timeoutPromise(3000);
       client.end();
     })
-    
-  }
-
-}
+  };
+} 
 
 export default Core;
